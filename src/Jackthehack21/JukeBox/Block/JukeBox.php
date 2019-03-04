8f9eef92cadd6b85bb7c3823ddab9a55ecdc1b89
Jackthehaxk21 <gangnam253@gmail.com>
2019-03-04 18:15:27 +0000

    Debug & added rest of records.

--- src/Jackthehack21/JukeBox/Block/JukeBox.php
+++ src/Jackthehack21/JukeBox/Block/JukeBox.php
@@ -8,6 +8,7 @@ use pocketmine\Player;
 use pocketmine\item\Item;
 use pocketmine\block\Solid;
 use pocketmine\block\BlockToolType;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 
 use Jackthehack21\JukeBox\Item\Record;
 
@@ -34,6 +35,7 @@ class JukeBox extends Solid{
     }
 
     public function onActivate(Item $item, Player $player = null) : bool{
+        printf("Activated\n");
         //Play / Stop sound.
         if(!$player instanceof Player){
             return false;
@@ -54,20 +56,31 @@ class JukeBox extends Solid{
         return parent::onBreak($item, $player);
     }
 
-    public function onIncinerate() : void{
-        $this->dropRecord();
-    }
-
     public function addRecord(Item $item) : void{
+        printf("Adding record\n");
         $this->record_inside = true;
         $this->record = $item;
+        $this->playSound($item->getSoundId());
     }
 
     public function dropRecord() : void{
         if($this->record_inside){
+            printf("Dropping record\n");
             $this->record_inside = false;
             $this->getLevel()->dropItem($this->asVector3(), $this->record);
             $this->record = null;
+            $this->stopSound();
         }
+    }
+
+    public function playSound(int $id) : void{
+        printf("%s\n",$id);
+        printf("Playing sound\n");
+        $this->getLevel()->broadcastLevelSoundEvent($this, $id);
+    }
+
+    public function stopSound() : void{
+        printf("Stopping sound\n");
+        $this->getLevel()->broadcastLevelSoundEvent($this, LevelSoundEventPacket::SOUND_STOP_RECORD);
     }
 }
--- src/Jackthehack21/JukeBox/Main.php
+++ src/Jackthehack21/JukeBox/Main.php
@@ -39,6 +39,17 @@ class Main extends PluginBase implements Listener
 
         //Records here:
         ItemFactory::registerItem(new Record(500, "13", LevelSoundEventPacket::SOUND_RECORD_13), true);
+		ItemFactory::registerItem(new Record(501, "Cat", LevelSoundEventPacket::SOUND_RECORD_CAT), true);
+		ItemFactory::registerItem(new Record(502, "Blocks", LevelSoundEventPacket::SOUND_RECORD_BLOCKS), true);
+		ItemFactory::registerItem(new Record(503, "Chirp", LevelSoundEventPacket::SOUND_RECORD_CHIRP), true);
+		ItemFactory::registerItem(new Record(504, "Far", LevelSoundEventPacket::SOUND_RECORD_FAR), true);
+		ItemFactory::registerItem(new Record(505, "Mall", LevelSoundEventPacket::SOUND_RECORD_MALL), true);
+		ItemFactory::registerItem(new Record(506, "Mellohi", LevelSoundEventPacket::SOUND_RECORD_MELLOHI), true);
+		ItemFactory::registerItem(new Record(507, "Stal", LevelSoundEventPacket::SOUND_RECORD_STAL), true);
+		ItemFactory::registerItem(new Record(508, "Strad", LevelSoundEventPacket::SOUND_RECORD_STRAD), true);
+		ItemFactory::registerItem(new Record(509, "Ward", LevelSoundEventPacket::SOUND_RECORD_WARD), true);
+		ItemFactory::registerItem(new Record(510, "11", LevelSoundEventPacket::SOUND_RECORD_11), true);
+		ItemFactory::registerItem(new Record(511, "Wait", LevelSoundEventPacket::SOUND_RECORD_WAIT), true);
 
         //Add to creative menu:
         Item::initCreativeItems();
